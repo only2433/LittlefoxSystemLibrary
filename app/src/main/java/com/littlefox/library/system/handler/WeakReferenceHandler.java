df29a989fecab0dacc61191844f6c5b6cd0d7935
only2433 <only2433@naver.com>
2019-08-21 11:52:14 +0900

    - 메모리 누수 없는 핸들러 추가

---
+++ app/src/main/java/com/littlefox/library/system/handler/WeakReferenceHandler.java
@@ -0,0 +1,36 @@
+package com.littlefox.library.system.handler;
+
+import android.os.Handler;
+import android.os.Message;
+import net.littlefox.lf_app_fragment.handler.callback.MessageHandlerCallback;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by 정재현 on 2017-12-28.
+ */
+
+public class WeakReferenceHandler extends Handler
+{
+    private WeakReference<MessageHandlerCallback> mHandlerActivity = null;
+
+    public WeakReferenceHandler(MessageHandlerCallback activity)
+    {
+        mHandlerActivity = new WeakReference<MessageHandlerCallback>(activity);
+    }
+
+    @Override
+    public void handleMessage(Message msg)
+    {
+        super.handleMessage(msg);
+
+        MessageHandlerCallback result = (MessageHandlerCallback)mHandlerActivity.get();
+
+        if(result == null)
+        {
+            return;
+        }
+
+        result.handlerMessage(msg);
+    }
+}
---
+++ app/src/main/java/com/littlefox/library/system/handler/callback/MessageHandlerCallback.java
@@ -0,0 +1,12 @@
+package com.littlefox.library.system.handler.callback;
+
+import android.os.Message;
+
+/**
+ * Created by 정재현 on 2017-12-28.
+ */
+
+public interface MessageHandlerCallback
+{
+    public void handlerMessage(Message message);
+}
