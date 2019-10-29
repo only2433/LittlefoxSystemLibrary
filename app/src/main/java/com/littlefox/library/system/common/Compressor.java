218a0105e8df10933f62b8f108768d49eb19de67
only2433 <only2433@naver.com>
2019-10-29 14:26:00 +0900

    - 압축 알고리즘 추가

---
+++ app/src/main/java/com/littlefox/library/system/common/Compressor.java
@@ -0,0 +1,91 @@
+package com.littlefox.library.system.common;
+
+import com.littlefox.logmonitor.Log;
+
+import net.lingala.zip4j.core.NativeStorage;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+
+import java.io.File;
+
+/**
+ * Created by only340 on 2018-01-30.
+ */
+
+public class Compressor {
+
+    public static boolean zip(String targetPath, String destinationFilePath)
+    {
+        return zip(targetPath, destinationFilePath ,"");
+    }
+
+    public static boolean zip(String targetPath, String destinationFilePath, String password)
+    {
+        try {
+            ZipParameters parameters = new ZipParameters();
+            parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+            parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+
+            if (password.equals("") == false)
+            {
+                parameters.setEncryptFiles(true);
+                parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
+                parameters.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+                parameters.setPassword(password);
+            }
+
+            ZipFile zipFile = new ZipFile(destinationFilePath);
+
+            File targetFile = new File(targetPath);
+            if (targetFile.isFile())
+            {
+                zipFile.addFile(targetFile, parameters);
+            }
+            else if (targetFile.isDirectory())
+            {
+                NativeStorage storage = new NativeStorage(targetFile);
+                zipFile.addFolder(storage, parameters);
+
+            }
+
+        } catch (Exception e)
+        {
+            Log.f("Exception : "+ e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean unzip(String targetZipFilePath, String destinationFolderPath)
+    {
+       return unzip(targetZipFilePath, destinationFolderPath, "");
+    }
+
+    public static boolean unzip(String targetZipFilePath, String destinationFolderPath, String password)
+    {
+        Log.i("targetZipFilePath : "+targetZipFilePath);
+        Log.i("destinationFolderPath : "+destinationFolderPath);
+        Log.i("password : "+password);
+        try {
+            ZipFile zipFile = new ZipFile(targetZipFilePath);
+
+            if(password.equals("") == false)
+            {
+                if (zipFile.isEncrypted())
+                {
+                    zipFile.setPassword(password);
+                }
+            }
+
+            zipFile.extractAll(destinationFolderPath);
+
+        } catch (Exception e)
+        {
+            Log.f("Exception : "+ e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+}
