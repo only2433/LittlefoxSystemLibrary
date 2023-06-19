dc05277db461c55bba3892fae46db772087c6e6a
only340 <only340@gmail.com>
2023-06-19 17:13:31 +0900

    [수정사항] - 테스트 커밋

--- app/src/main/java/com/littlefox/library/system/common/Common.java
+++ app/src/main/java/com/littlefox/library/system/common/Common.java
@@ -5,4 +5,5 @@ public class Common
 	public static final int ICE_CREAM_SANDWICH 	= 14;
 	public static final int JELLYBEAN_CODE 		= 16;
 	public static final int JELLYBEAN_CODE_4_3 	= 18;
+	public static final int KITKAT			    = 19;
 }
